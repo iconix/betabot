82d7f4eed62d9e30682137c91cb66fcf3f2a32de
Mikhail Simin <mikhail.simin@gmail.com>
2019-08-30 11:59:40 -0700

    Add uptime to default commands

--- alphabot/default-scripts/generic.py
+++ alphabot/default-scripts/generic.py
@@ -1,35 +1,13 @@
-import logging
+import time
 
 import alphabot.bot
+
+import logging
 
 bot = alphabot.bot.get_instance()
 log = logging.getLogger(__name__)
 
-
-@bot.add_command('!help$')
-@bot.learn([
-    'List your commands',
-    'What can you do?',
-    'What commands do you know?',
-    'help'])
-async def help(message):
-    """Get summary of all commands
-
-    Usage: !help
-    """
-    help_text = _make_help_text(bot.help.list())
-    await message.reply(help_text)
-
-
-@bot.add_command('!help (.*)')
-async def help_query(message):
-    """Get detailed help of a command
-
-    Usage: !help <command>
-    """
-    query = message.regex_groups[0]
-    help_text = _make_help_text(bot.help.list(query))
-    await message.reply(help_text)
+START_TIME = time.time()
 
 
 def _make_help_text(help_list):
@@ -40,3 +18,61 @@ def _make_help_text(help_list):
         else:
             reply += '`%s`\n' % usage
     return reply
+
+
+@bot.add_command('!help$')
+@bot.add_command('^help$', direct=True)
+@bot.learn([
+    'List your commands',
+    'What can you do?',
+    'What commands do you know?'])
+async def generic_help(message):
+    """Get summary of all commands
+
+    Usage: !help
+    """
+
+    await message.reply('Here are my commands, but I can also understand natural language. '
+                        'Just ask me "What can you do?"')
+    help_text = _make_help_text(bot.help.list())
+    await message.reply(help_text)
+
+
+@bot.add_command('!help (.*)')
+@bot.add_command('^help (.*)', direct=True)
+async def help_query(message):
+    """Get detailed help of a command
+
+    Usage: !help <command>
+    """
+    query = message.regex_groups[0]
+    help_text = _make_help_text(bot.help.list(query))
+    await message.reply(help_text)
+
+
+@bot.add_command('uptime')
+@bot.learn(['How long have you been running?', "What's your uptime?"])
+async def get_uptime(message):
+    """Get amount of time this bot has been running
+
+    Usage: uptime
+    """
+
+    runtime = int(time.time()) - int(START_TIME)
+
+    if runtime < 60:
+        await message.reply(f'{runtime} seconds')
+        return
+
+    runtime //= 60
+    if runtime < 60:
+        await message.reply(f'{runtime} minutes')
+        return
+
+    runtime //= 24
+    if runtime < 24:
+        await message.reply(f'{runtime} hours')
+        return
+
+    await message.reply(f'{runtime} days')
+    return
